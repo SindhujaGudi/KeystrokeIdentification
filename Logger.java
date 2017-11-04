632dac620b8de6175cbddd6bc933791b67d48447
Sindhuja Gudi <31775417+SindhujaGudi@users.noreply.github.com>
2017-11-04 13:53:07 -0500

    Update Logger.java

--- Logger.java
+++ Logger.java
@@ -1,10 +1,15 @@
+/*
+This file has methods for logging info and error messages.
+It displays the messages on the console.
+ */
 public class Logger {
-    public static void info(String message) {
-        System.out.println(message);
+    public static void info(String message, Object... params) {
+        System.out.println(String.format(message, params));
     }
 
     public static void error(String message, Exception e) {
         System.err.printf("%s. Error - %s\n", message, e.getMessage());
+        e.printStackTrace();
     }
 
     public static void error(Exception e) {
@@ -13,5 +18,13 @@ public class Logger {
 
     public static void error(String message) {
         System.err.println(message);
+    }
+
+    public static void sectionEnd() {
+        info("=====================");
+    }
+
+    public static void logScore(double score) {
+        info(String.format("Score is %.2f", score));
     }
 }
