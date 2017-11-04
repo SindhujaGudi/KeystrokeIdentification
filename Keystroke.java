eca020a5fab6f20731d225f72bd2d6290c79885b
Sindhuja Gudi <31775417+SindhujaGudi@users.noreply.github.com>
2017-11-04 13:52:33 -0500

    Update Keystroke.java

--- Keystroke.java
+++ Keystroke.java
@@ -1,17 +1,49 @@
+import java.util.Objects;
+
+/*
+A class to record the user typed key and its startTime of even
+ */
 public class KeyStroke {
     private String key;
-    private long time;
+    private long startTime;
+    private long endTime;
 
-    public KeyStroke(String key, long time) {
+    public KeyStroke(String key, long startTime) {
         this.key = key;
-        this.time = time;
+        this.startTime = startTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KeyStroke keyStroke = (KeyStroke) o;
+        return Objects.equals(key, keyStroke.key);
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key);
     }
 
     public String getKey() {
         return key;
     }
 
-    public long getTime() {
-        return time;
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
     }
 }
